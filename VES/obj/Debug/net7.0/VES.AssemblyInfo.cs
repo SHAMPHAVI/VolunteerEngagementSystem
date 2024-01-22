d2512d0e5e311e19a386a4779177828d6b119c95
Shamphavi Shanmugasundram <s_shamphavi@outlook.com>
2024-01-22 23:02:33 +0530

    engagement Dashboard

--- VES/obj/Debug/net7.0/VES.AssemblyInfo.cs
+++ VES/obj/Debug/net7.0/VES.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VES")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec6222820cd2e21b548b74cdc488e45a6f4802c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f4897ad81944f327898e88d0b4fbcc85b805cad")]
 [assembly: System.Reflection.AssemblyProductAttribute("VES")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VES")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
